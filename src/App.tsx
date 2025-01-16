7d2c3e1bc6704e3182f8184fabf56c4ebe580b41
Saheed Babayomi <saheedbabayomi22@gmail.com>
2025-01-16 17:13:38 +0100

    add files

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+function App() {
+const [count, setCount] = useState(0)
+function increase(){
+  setCount(prev => prev + 1)
+}
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick= {increase}>Increase</button>
+   </div>
+  )
+}
+
+export default App
