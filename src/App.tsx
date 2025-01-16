e808364de14b34f4852c2633e18fcbfed0168426
Saheed Babayomi <saheedbabayomi22@gmail.com>
2025-01-16 18:29:29 +0100

    added Todo.jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import { useState } from 'react'
+import Todo from "./components/Todo.jsx"
 
 function App() {
-const [count, setCount] = useState(0)
-function increase(){
-  setCount(prev => prev + 1)
-}
   return (
     <div>
-      <h1>{count}</h1>
-      <h1>added new line of code</h1>
-      <button onClick= {increase}>Increase</button>
+      hh
+   <Todo/>
    </div>
   )
 }
